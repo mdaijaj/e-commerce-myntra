6aefad9d6672be94730278c52ce2d52965d0010b
mdaijaj <aijaj18@navgurukul.org>
2021-11-19 09:33:29 +0530

    add file

--- backend/models/category_schema.js
+++ backend/models/category_schema.js
@@ -3,13 +3,19 @@ const Schema = mongoose.Schema;
 
 const categorySchema=new Schema({
     category_name: {
-        type: String
+        type: String,
+        required: true,
+        unique: true
     },
     createAt: {
         type: Date,
         default: Date.now,
+    },
+    id: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Product"
     }
 });
 
-const Category=mongoose.model('Category', categorySchema);
+const Category=mongoose.model('Category_model', categorySchema);
 module.exports= Category;
